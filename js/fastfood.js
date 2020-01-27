413f4dd807c65d7982028042d47c368be65ecfda
3dmultimedia <59264531+3dmultimedia@users.noreply.github.com>
2020-01-27 03:25:42 +0300

    Add files via upload

---
+++ js/fastfood.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+	
+	$("#fastfood").vc3dEye({
+		imagePath:"images/fastfood/",
+		totalImages: 40,
+		imageExtension:"jpg"
+	});
+	
+});
+
---
+++ js/shopcenter.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+	
+	$("#shopcenter").vc3dEye({
+		imagePath:"images/shopcent/",
+		totalImages: 50,
+		imageExtension:"jpg"
+	});
+	
+});
